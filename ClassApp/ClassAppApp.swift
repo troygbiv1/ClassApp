487d078e75ffbe1a5397b60b7360fc7d37b8ce4e
troygbiv1 <22031473+troygbiv1@users.noreply.github.com>
2023-06-20 00:28:32 +0700

    Initial Commit

---
+++ ClassApp/ClassAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassAppApp.swift
+//  ClassApp
+//
+//  Created by Troy Volanth on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ClassAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
